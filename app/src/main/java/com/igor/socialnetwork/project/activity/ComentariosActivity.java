ac80a80a76953841eaa1dd45fd156092f91c4b46
unknown <igormelo9001@gmail.com>
2019-08-15 13:25:55 -0300

    recovered comments

--- app/src/main/java/com/igor/socialnetwork/project/activity/ComentariosActivity.java
+++ app/src/main/java/com/igor/socialnetwork/project/activity/ComentariosActivity.java
@@ -5,19 +5,37 @@ import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.ValueEventListener;
 import com.igor.socialnetwork.project.R;
+import com.igor.socialnetwork.project.adapter.AdapterComentario;
+import com.igor.socialnetwork.project.helper.ConfiguracaoFirebase;
 import com.igor.socialnetwork.project.helper.UsuarioFirebase;
 import com.igor.socialnetwork.project.model.Comentario;
 import com.igor.socialnetwork.project.model.Usuario;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ComentariosActivity extends AppCompatActivity {
 
     private EditText editComentario;
+    private RecyclerView recyclerComentarios;
     private String idPostagem;
     private Usuario usuario;
+    private AdapterComentario adapterComentario;
+    private List<Comentario> listaComentarios = new ArrayList<>();
+    private DatabaseReference firebaseRef;
+    private DatabaseReference comentariosRef;
+    private ValueEventListener valueEventListenerComentarios;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,14 +49,57 @@ public class ComentariosActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
         editComentario = findViewById(R.id.editComentario);
+        recyclerComentarios = findViewById(R.id.recyclerComentarios);
+
+        adapterComentario = new AdapterComentario(listaComentarios, getApplicationContext());
+        recyclerComentarios.setHasFixedSize(true);
+        recyclerComentarios.setLayoutManager(new LinearLayoutManager(this));
+        recyclerComentarios.setAdapter(adapterComentario);
 
         usuario = UsuarioFirebase.getDadosUsuarioLogado();
+        firebaseRef = ConfiguracaoFirebase.getFirebase();
 
         Bundle bundle = getIntent().getExtras();
         if(bundle != null){
             idPostagem = bundle.getString("idPostagem");
         }
 
+    }
+
+    private void recuperarComentarios(){
+
+        comentariosRef = firebaseRef.child("comentarios")
+                .child(idPostagem);
+        valueEventListenerComentarios = comentariosRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                listaComentarios.clear();
+
+                for(DataSnapshot ds: dataSnapshot.getChildren()){
+                    listaComentarios.add(ds.getValue(Comentario.class));
+                }
+
+                adapterComentario.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        recuperarComentarios();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        comentariosRef.removeEventListener(valueEventListenerComentarios);
     }
 
     public void salvarComentario(View view){
