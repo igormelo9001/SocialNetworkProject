8ce4dadf32e17a1cb99f868744885cd676f4f6d8
unknown <igormelo9001@gmail.com>
2019-08-15 12:36:27 -0300

    saved comment

--- app/src/main/java/com/igor/socialnetwork/project/activity/ComentariosActivity.java
+++ app/src/main/java/com/igor/socialnetwork/project/activity/ComentariosActivity.java
@@ -1,13 +1,23 @@
 package com.igor.socialnetwork.project.activity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
 import com.igor.socialnetwork.project.R;
+import com.igor.socialnetwork.project.helper.UsuarioFirebase;
+import com.igor.socialnetwork.project.model.Comentario;
+import com.igor.socialnetwork.project.model.Usuario;
 
 public class ComentariosActivity extends AppCompatActivity {
+
+    private EditText editComentario;
+    private String idPostagem;
+    private Usuario usuario;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,11 +30,43 @@ public class ComentariosActivity extends AppCompatActivity {
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
+        editComentario = findViewById(R.id.editComentario);
+
+        usuario = UsuarioFirebase.getDadosUsuarioLogado();
+
+        Bundle bundle = getIntent().getExtras();
+        if(bundle != null){
+            idPostagem = bundle.getString("idPostagem");
+        }
+
+    }
+
+    public void salvarComentario(View view){
+
+        String textoComentario = editComentario.getText().toString();
+        if(textoComentario != null && !textoComentario.equals("")){
+
+            Comentario comentario = new Comentario();
+            comentario.setIdPostagem(idPostagem);
+            comentario.setIdUsuario(usuario.getId());
+            comentario.setNomeUsuario(usuario.getNome());
+            comentario.setCaminhoFoto(usuario.getCaminhoFoto());
+            comentario.setComentario(textoComentario);
+            if(comentario.salvar()){
+                Toast.makeText(this, "Comentário salvo com sucesso!", Toast.LENGTH_SHORT).show();
+            }
+
+        }else {
+            Toast.makeText(ComentariosActivity.this, "Insira o comentário antes de salvar", Toast.LENGTH_SHORT).show();
+        }
+
+        editComentario.setText("");
+
     }
 
     @Override
     public boolean onSupportNavigateUp() {
         finish();
-        return super.onSupportNavigateUp();
+        return false;
     }
 }
